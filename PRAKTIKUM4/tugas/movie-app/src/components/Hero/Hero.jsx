828b84d1719673cfec389bb3d7346b32d5316177
Shofi Syahria Mahram <shofishifa31@gmail.com>
2024-06-03 09:54:27 +0700

    practice - effect & hook

--- PRAKTIKUM4/tugas/movie-app/src/components/Hero/Hero.jsx
+++ PRAKTIKUM4/tugas/movie-app/src/components/Hero/Hero.jsx
@@ -1,31 +1,43 @@
+import { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
-    return (
-        <div className={styles.container}>
-            <section className={styles.hero}>
-                <div className={styles.hero__left}>
-                    <h2 className={styles.hero__title}>Spiderman</h2>
-                    <h3 className={styles.hero__genre}>
-                        Genre: Action, Fantasy
-                    </h3>
-                    <p className={styles.hero__description}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
-                    </p>
-                    <button className={styles.hero__button}>Watch</button>
-                </div>
-                <div className={styles.hero__right}>
-                    <img
-                        className={styles.hero__image}
-                        src="https://picsum.photos/536/354"
-                        alt="placeholder"
-                    />
-                </div>
-            </section>
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const response = await fetch("https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590");
+
+    const data = await response.json();
+
+    setMovies(data);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.hero}>
+        <div className={styles.hero__left}>
+          <h2 className={styles.hero__title}>{movies.Title}</h2>
+          <h3 className={styles.hero__genre}>Genre: {movies.Genre}</h3>
+          <p className={styles.hero__description}>
+            {movies.Plot}
+          </p>
+          <button className={styles.hero__button}>Watch</button>
         </div>
-    );
+        <div className={styles.hero__right}>
+          <img
+            className={styles.hero__image}
+            src={movies.Poster}
+            alt={movies.Title}
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
