1f11654e0fa6bde1a7e005a675ccb5af61eac024
raihandotmd <justraihan@proton.me>
2024-07-08 15:55:05 +0700

    task - building the UI

---
+++ PRAKTIKUM4/tugas/movie-app/src/components/Form/Form.jsx
@@ -0,0 +1,19 @@
+import styles from "./Form.module.css"; // Import CSS Modules
+
+const MovieAddForm = ({ onSubmit }) => {
+  return (
+    <form className={styles.form}>
+      <h2>Add Movie</h2>
+      <label htmlFor="title">Title:</label>
+      <input id="title" type="text" className={styles.titleInput} />
+
+      <label htmlFor="year">Year:</label>
+      <input id="year" type="text" className={styles.genreInput} />
+      <button type="submit" className={styles.submitButton}>
+        Add Movie
+      </button>
+    </form>
+  );
+};
+
+export default MovieAddForm;
--- PRAKTIKUM4/tugas/movie-app/src/pages/Home.jsx
+++ PRAKTIKUM4/tugas/movie-app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Hero from "../components/Hero/Hero";
 import Movies from "../components/Movies/Movies";
+import MovieAddForm from "../components/Form/Form";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <main>
         <Hero />
         <Movies />
+        <MovieAddForm />
       </main>
       <Footer />
     </div>
